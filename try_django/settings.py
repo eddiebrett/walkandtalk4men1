9d21f9685f93d36674cc8d43c1bf73a7d4fdf6bd
Ubuntu <ubuntu@ip-172-31-6-252.eu-west-1.compute.internal>
2019-09-26 17:56:04 +0000

    added s3 deatils to setting and to env

--- try_django/settings.py
+++ try_django/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [ # components
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'storages',
     'blog',
     'searches',
 ]
